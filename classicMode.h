75f8a1be0094de9a96f2c9e0300a949b3ecea6ba
kashishpandey <56780262+kashishpandey@users.noreply.github.com>
2020-10-03 17:59:13 -0700

    Create classicMode.h

---
+++ classicMode.h
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+class ClassicMode{
+
+  //member variables
+  private:
+    int neighbor;
+    int row;
+    int column;
+
+    char *gameGrid1;
+    char *gameGrid2;
+    
+
+
+  //methods
+  public:
+    ClassicMode();
+    ClassicMode(int row, int column, gameGrid1);
+    ~ClassicMode();
+    bool isEqual();
+};
